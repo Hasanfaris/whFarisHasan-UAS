e6f9b340861a0198a6373cd87591128d8578a366
Hasanfaris <farishasan755@gmail.com>
2025-05-31 17:04:51 +0700

    nambahin gambar & update file data

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+   static String nama = "Faris";
+    static String gambar = "assets/faris.jpg";
 }
